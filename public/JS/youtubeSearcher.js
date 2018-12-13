c322cb61ab3ee199d870156f245c522ff8815614
Bryan Cancel <bryan.o.cancel@gmail.com>
2018-12-13 02:58:13 -0600

    added rerun query button

--- public/JS/youtubeSearcher.js
+++ public/JS/youtubeSearcher.js
@@ -13,6 +13,75 @@ $(document).ready(function() {
         return string
     }
 
+    function runLyricSearch(){
+        //show lyrics section
+        $("#lyricContainer").show()
+
+        //grab data (if nothing then == "")
+        song = $(songName).val()
+        album = $(albumName).val()
+        artist = $(artistName).val()
+
+        //output result
+        if(song != "" && artist != ""){
+            console.log("searching for lyrics") //For "is it still working?" checks
+
+            //show loading... eventually we will flip this below...
+            $("#lyricsLoading").show()
+            $("#lyricsLoaded").hide()
+
+            $.ajax({
+                method: "POST",
+                url: "/getLyrics",
+                data: {
+                    "song" : song,
+                    "album" : album,
+                    "artist" : artist
+                },
+                success: function (lyrics) {
+                    console.log("lyric search complete successfully") //For "is it still working?" checks
+
+                    //create message
+                    //replace new line characters for the new line character of html "<br>"
+                    var regex = /\\n/gi
+                    lyrics = lyrics.replace(regex, "<br>")
+
+                    //show loaded
+                    $("#lyricsLoading").hide()
+                    $("#lyricsLoaded").show()
+
+                    //display message
+                    $("#lyricsLoaded").html(lyrics)
+                },
+                error: function (jqXHR, textStatus, errorThrown) { 
+                    console.log("lyric search complete un-successfully") //For "is it still working?" checks
+
+                    //create message
+                    var message = 'Lyrics for ' + makeQueryString(song, album, artist) + ' Not Found'
+                    + '<br>' + "The song might be to new"
+                    + '<br>' + "Song Lyrics are retreived from MetroLyrics"
+                    + '<br>' + "Make sure the song lyrics are available there"
+                    + '<br>' + "If they are, make sure your spelling is correct"
+
+                    //show loaded
+                    $("#lyricsLoading").hide()
+                    $("#lyricsLoaded").show()
+
+                    //display message
+                    $("#lyricsLoaded").html(message)
+                }
+            })
+        }
+        else{
+            //show as loaded
+            $("#lyricsLoading").hide()
+            $("#lyricsLoaded").show()
+
+            //show lyrics in html
+            $("#lyricsLoaded").html('In order to find the lyrics for ' + makeQueryString(song, album, artist) + ' (1) Artist Name AND (2) Song Name are required')
+        }
+    }
+
     function linkUpdaters(textField){
         //generate youtube link
         $(textField).focusout(function() {
@@ -40,78 +109,18 @@ $(document).ready(function() {
 
         //display lyrics
         $(textField).focusout(function() {
-            //show lyrics section
-            $("#lyricContainer").show()
-
-            //grab data (if nothing then == "")
-            song = $(songName).val()
-            album = $(albumName).val()
-            artist = $(artistName).val()
-
-            //output result
-            if(song != "" && artist != ""){
-                console.log("searching for lyrics") //For "is it still working?" checks
-
-                //show loading... eventually we will flip this below...
-                $("#lyricsLoading").show()
-                $("#lyricsLoaded").hide()
-
-                $.ajax({
-                    method: "POST",
-                    url: "/getLyrics",
-                    data: {
-                        "song" : song,
-                        "album" : album,
-                        "artist" : artist
-                    },
-                    success: function (lyrics) {
-                        console.log("lyric search complete successfully") //For "is it still working?" checks
-    
-                        //create message
-                        //replace new line characters for the new line character of html "<br>"
-                        var regex = /\\n/gi
-                        lyrics = lyrics.replace(regex, "<br>")
-    
-                        //show loaded
-                        $("#lyricsLoading").hide()
-                        $("#lyricsLoaded").show()
-    
-                        //display message
-                        $("#lyricsLoaded").html(lyrics)
-                    },
-                    error: function (jqXHR, textStatus, errorThrown) { 
-                        console.log("lyric search complete un-successfully") //For "is it still working?" checks
-
-                        //create message
-                        var message = 'Lyrics for ' + makeQueryString(song, album, artist) + ' Not Found'
-                        + '<br>' + "The song might be to new"
-                        + '<br>' + "Song Lyrics are retreived from MetroLyrics"
-                        + '<br>' + "Make sure the song lyrics are available there"
-                        + '<br>' + "If they are, make sure your spelling is correct"
-
-                        //show loaded
-                        $("#lyricsLoading").hide()
-                        $("#lyricsLoaded").show()
-
-                        //display message
-                        $("#lyricsLoaded").html(message)
-                    }
-                })
-            }
-            else{
-                //show as loaded
-                $("#lyricsLoading").hide()
-                $("#lyricsLoaded").show()
-
-                //show lyrics in html
-                $("#lyricsLoaded").html('In order to find the lyrics for ' + makeQueryString(song, album, artist) + ' (1) Artist Name AND (2) Song Name are required')
-            }
+            runLyricSearch()
         })
     }
 
     linkUpdaters(songName)
     linkUpdaters(albumName)
     linkUpdaters(artistName)
+
+    $("#lyricSearchButton").on("click", function(event){
+        event.preventDefault();
+        runLyricSearch()
+    })
 
     recordAudio = $("#recordAudio")
     recording = false
