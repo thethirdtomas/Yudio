a12aa9516978a0f88709e174efd35035b85012f5
Bryan Cancel <bryan.o.cancel@gmail.com>
2018-12-13 03:26:09 -0600

    downloadable audio implemented... currently not openable... repair required

--- public/JS/audioVisualizer.js
+++ public/JS/audioVisualizer.js
@@ -1,4 +1,10 @@
-window.onload = function () {
+$( document ).ready(function() {
+    //variables that will let us save recorded audio
+    var userMedia = ""
+    var mediaRecorder = ""
+    var audioChunks = []
+
+    //variables for audio visualizer
     "use strict";
     var paths = document.getElementsByTagName('path');
     var visualizer = document.getElementById('visualizer');
@@ -7,7 +13,7 @@ window.onload = function () {
     var path;
     var report = 0;
     
-    var soundAllowed = function (stream) {
+    var soundDesired = function (stream) {
         //Audio stops listening in FF without // window.persistAudioStream = stream;
         //https://bugzilla.mozilla.org/show_bug.cgi?id=965483
         //https://support.mozilla.org/en-US/questions/984179
@@ -21,7 +27,7 @@ window.onload = function () {
         var frequencyArray = new Uint8Array(analyser.frequencyBinCount);
         visualizer.setAttribute('viewBox', '0 0 255 255');
       
-				//Through the frequencyArray has a length longer than 255, there seems to be no
+        //Through the frequencyArray has a length longer than 255, there seems to be no
         //significant data after this point. Not worth visualizing.
         for (var i = 0 ; i < 255; i++) {
             path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
@@ -51,6 +57,48 @@ window.onload = function () {
                               navigator.webkitGetUserMedia ||
                               navigator.mozGetUserMedia    ||
                               null;*/
-    navigator.getUserMedia({audio:true}, soundAllowed, soundNotAllowed);
+    navigator.getUserMedia({audio:true}, soundDesired, soundNotAllowed)
 
-};
+    navigator.mediaDevices.getUserMedia({audio:true}).then((stream) => {
+        //create a recorder that is available for manuipulation
+        mediaRecorder = new MediaRecorder(stream)
+
+        mediaRecorder.ondataavailable = function (event){
+            audioChunks.push(event.data)
+        }
+    })
+
+    var recordAudio = $("#recordAudio")
+    var recording = false
+
+    $(recordAudio).on("click", function(){
+        console.log("recording " + recording)
+        var start = "fa-microphone"
+        var stop = "fa-microphone-slash"
+        $(this).find("i").removeClass(((recording) ? stop : start))
+        $(this).find("i").addClass(((recording) ? start : stop))
+        recording = !recording
+
+        //start recording
+        if(recording) mediaRecorder.start()
+        else{ //stop recording
+            mediaRecorder.stop()
+
+            //audio chunk conversion
+            const audioBlob = new Blob(audioChunks, { type: 'audio/mpeg' })
+            const audioUrl = URL.createObjectURL(audioBlob)
+            const audio = new Audio(audioUrl)
+
+            console.log(audioUrl)
+
+            //link hacking to save the file
+            var a = document.createElement('a')
+            document.body.appendChild(a)
+            a.style = 'display: none'
+            a.href = audioUrl
+            a.download = 'test.mp3'
+            a.click()
+            a.remove()
+        }
+    })
+})
--- public/JS/youtubeSearcher.js
+++ public/JS/youtubeSearcher.js
@@ -121,33 +121,4 @@ $(document).ready(function() {
         event.preventDefault();
         runLyricSearch()
     })
-
-    recordAudio = $("#recordAudio")
-    recording = false
-
-    $(recordAudio).on("click", function(){
-        console.log("recording " + recording)
-        start = "fa-microphone"
-        stop = "fa-microphone-slash"
-        $(this).find("i").removeClass(((recording) ? stop : start))
-        $(this).find("i").addClass(((recording) ? start : stop))
-        recording = !recording
-    })
 })
-
-/*
-//hide lyrics of previous query
-//we do this because searching for lyrics takes a while
-$("#lyricContainer").hide()
-
-console.log("searching for lyrics")
-
-lyrics = `I used to rule the world\nSeas would rise when I gave the word\nNow in the morning I sleep alone\nSweep the streets I used to own\nI used to roll the dice\nFeel the fear in my enemy's eyes\nListen as the crowd would sing\n"Now the old king is dead! Long live the king!"\nOne minute I held the key\nNext the walls were closed on me\nAnd I discovered that my castles stand\nUpon pillars of salt and pillars of sand\nI hear Jerusalem bells a-ringing\nRoman Cavalry choirs are singing\nBe my mirror, my sword and shield\nMy missionaries in a foreign field\nFor some reason I can't explain\nOnce you'd gone there was never\nNever an honest word\nAnd that was when I ruled the world\nIt was the wicked and wild wind\nBlew down the doors to let me in\nShattered windows and the sound of drums\nPeople couldn't believe what I'd become\nRevolutionaries wait\nFor my head on a silver plate\nJust a puppet on a lonely string\nOh who would ever want to be king?\nI hear Jerusalem bells a-ringing\nRoman cavalry choirs are singing\nBe my mirror, my sword and shield\nMy missionaries in a foreign field\nFor some reason I can't explain\nI know Saint Peter won't call my name\nNever an honest word\nBut that was when I ruled the world\nI hear Jerusalem bells a-ringing\nRoman cavalry choirs are singing\nBe my mirror, my sword and shield\nMy missionaries in a foreign field\nFor some reason I can't explain\nI know Saint Peter won't call my name\nNever an honest word\nBut that was when I ruled the world`
-lyrics = lyrics.replace(/(\r\n\t|\n|\r\t)/gm,"<br>");
-
-//show the lyrics area
-$("#lyricContainer").show()
-
-//set html so that it read <br> tags between lyric lines
-$("#lyricsArea").html(lyrics)
-*/
